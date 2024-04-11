40b4e076747386bea1413494ac034c2b3000a557
Pedro Galembeck <galembeckpedro@gmail.com>
2024-04-11 12:46:09 -0300

    feat: create footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+export function Footer() {
+  return (
+    <div className="footer flex items-center justify-between p-[30px]">
+      <span className="font-bold">uifry</span>
+      <span className="text-[14px]">
+        © 2024 |
+        <a href="https://github.com/galembeck" target="_blank" className="hover:text-slate-500"> Pedro Galembeck</a>
+      </span>
+    </div>
+  );
+}
