a34a154ade85daa2b66af3d6ccf61ef343d3bf87
Pedro Galembeck <galembeckpedro@gmail.com>
2024-04-12 17:24:58 -0300

    refactor: update dynamic menu categories

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,9 +1,21 @@
+import { Link } from "react-router-dom";
+
+import { menu } from '../data';
+
 export function Menu() {
   return (
     <div className="menu">
-      <div className="item">
-        <span className="title">MAIN</span>
-      </div>
+      {menu.map((item) => (
+        <div key={item.id} className="item flex flex-col gap-[10px] mb-[20px]">
+          <span className="title text-xs font-extralight text-text-soft-color">{item.title}</span>
+          {item.listItems.map((listItem) => (
+            <Link to={listItem.url} key={listItem.id} className="item flex items-center gap-[10px] p-[10px] rounded-[5px] hover:bg-soft-background">
+              <img src={listItem.icon} alt="Icon" />
+              <span className="item-title">{listItem.title}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
